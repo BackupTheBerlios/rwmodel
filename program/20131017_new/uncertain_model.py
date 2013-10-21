5d6694ae732c501241bd9ac483497e848dff4032
--global <--global>
2013-10-21 21:41:52 +0200

    uncertain model updated

--- program/20131017_new/uncertain_model.py
+++ program/20131017_new/uncertain_model.py
@@ -15,23 +15,22 @@ from scipy.stats import beta
 UM = []
 
 #P(I^t_(x,y)|C^t piC)
-def symetrical_beta_dist(t, x, y):
-  result = 1
+def symetrical_beta_dist(TM, t, x, y):
   param= 0.075
   
-  if g_x == x and g_y == y:
-    return beta.cdf(I_T,:0.075, 0.075)
-  else:
-    return numpy.random.uniform()
-  return result
+  for b in xrange(blocks):
+    if (x,y) not in gaze_position[b][t]:
+      continue
+    else:
+      return beta.cdf(TM[t][x][y],0.075, 0.075)
+  return numpy.random.uniform()
 #probability of gaze position
 def tm(TM):
   UM = TM
   for t in range(2, times+1):
-    print 'time', t
     for x in xrange(X):
       for y in xrange(Y):
-        UM[t][x][y] *= symetrical_beta_dist(t, x, y)
+        UM[t][x][y] *= symetrical_beta_dist(TM, t, x, y)
   
   return UM
 
